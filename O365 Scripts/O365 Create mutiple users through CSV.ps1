f4923c732315ef48696c3eb5e3edc4b3e1429ea7
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 17:38:28 -0600

    O365 Automation - Create multiple users at a time
    Keep in mind you will need a CSV file for this and save it to a destination. Once you do, you will need access to the Tenant.  Remember the save file and how much licenses you have on hand. This should allow you to create multiple users in O365 Tenant.

---
+++ O365 Scripts/O365 Create mutiple users through CSV.ps1
@@ -0,0 +1,16 @@
+﻿# Import user data from CSV file
+$Users = Import-Csv -Path "C:\Users\YourUsername\Documents\NewUsers.csv"
+
+# Create users and assign licenses
+foreach ($User in $Users) {
+    $UserPrincipalName = $User.UserPrincipalName
+    $DisplayName = $User.DisplayName
+    $Password = $User.Password
+    $License = "yourdomain:ENTERPRISEPACK"
+
+    # Create the new user
+    New-MsolUser -UserPrincipalName $UserPrincipalName -DisplayName $DisplayName -Password $Password
+
+    # Assign a license to the new user
+    Set-MsolUserLicense -UserPrincipalName $UserPrincipalName -AddLicenses $License
+}
