2c2714716b26bdf0b4aa416a3821465d77daf221
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 17:56:36 -0600

    Sites list for a Tenant
    Make sure you modify the code as you see fit!

---
+++ O365 Scripts/Getting all sites for OneDrive Tenant.ps1
@@ -0,0 +1,5 @@
+﻿# Connect to SharePoint Online
+Connect-SPOService -Url https://yourdomain-admin.sharepoint.com
+
+# Get all OneDrive sites
+Get-SPOSite -Template "SPSPERS" | Select-Object Url, Owner
