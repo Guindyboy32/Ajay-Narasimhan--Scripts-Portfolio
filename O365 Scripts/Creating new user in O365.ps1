44160794c3cfc7d0d0057890eacd1d8af66ca80d
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 17:05:42 -0600

    Creating a user in O365
    Code will allow you to create a user in O365 with the username, password, and license.

---
+++ O365 Scripts/Creating new user in O365.ps1
@@ -0,0 +1,11 @@
+﻿# Define user details
+$UserPrincipalName = "newuser@yourdomain.com"
+$DisplayName = "New User"
+$Password = "P@ssw0rd"
+
+# Create the new user
+New-MsolUser -UserPrincipalName $UserPrincipalName -DisplayName $DisplayName -Password $Password
+
+# Assign a license to the new user
+$License = "yourdomain:ENTERPRISEPACK"
+Set-MsolUserLicense -UserPrincipalName $UserPrincipalName -AddLicenses $License
