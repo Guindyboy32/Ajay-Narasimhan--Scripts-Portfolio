f03ad2edbd6f25f75b99812c60f8436d971a600a
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 11:30:53 -0600

    Script to Automate VM Scaling Based on CPU Usage
    Stress test that VM in the sandbox, you can see it add another VM by one (Autoscaling) 
    
    You can do this by Nerdio, or even Azure, AWS, and GCP as well when setting up VMs. 
    
    https://learn.microsoft.com/en-us/azure/azure-monitor/autoscale/autoscale-overview

---
+++ Script to Automate VM Scaling Based on CPU Usage.ps1
@@ -0,0 +1,18 @@
+﻿# Define parameters
+$resourceGroupName = "MyResourceGroup"
+$vmScaleSetName = "MyVMSS"
+$cpuThreshold = 75
+
+# Get the VM scale set
+$vmss = Get-AzVmss -ResourceGroupName $resourceGroupName -VMScaleSetName $vmScaleSetName
+
+# Check CPU usage and scale up if necessary
+$metrics = Get-AzMetric -ResourceId $vmss.Id -MetricName "Percentage CPU" -TimeGrain "PT1M" -StartTime (Get-Date).AddMinutes(-10) -EndTime (Get-Date)
+$averageCpu = ($metrics.Data | Measure-Object -Property Average -Average).Average
+if ($averageCpu -gt $cpuThreshold) {
+    $vmss.Sku.Capacity += 1
+    Update-AzVmss -ResourceGroupName $resourceGroupName -VMScaleSetName $vmScaleSetName -Sku $vmss.Sku
+    Write-Output "Scaled up VMSS by one instance due to high CPU usage."
+} else {
+    Write-Output "CPU usage is within the threshold."
+}
