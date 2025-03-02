b31b34f3ca5ad19386e43053954f35cd03478c8c
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 17:07:54 -0600

    Reset User O365 Password
    Substitute the values and you should be good!

---
+++ O365 Scripts/Reset user O365 password.ps1
@@ -0,0 +1,6 @@
+﻿# Define user details
+$UserPrincipalName = "user@yourdomain.com"
+$NewPassword = "N3wP@ssw0rd"
+
+# Reset the user's password
+Set-MsolUserPassword -UserPrincipalName $UserPrincipalName -NewPassword $NewPassword -ForceChangePassword $false
