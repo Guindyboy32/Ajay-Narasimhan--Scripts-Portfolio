53625aeb93967a7e3fa7140ddd6f776b56246917
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 10:14:14 -0600

    Creating User Account in Domain Controller
    You can run this script in RMM by assigning variables to the script. 
    
    You can also run this traditionally in your DC as well and plug in the values.

---
+++ Creating user accounts..ps1
@@ -0,0 +1,16 @@
+﻿# Define user properties
+$Username = "jdoe"
+$Password = ConvertTo-SecureString "P@ssw0rd!" -AsPlainText -Force
+$DisplayName = "John Doe"
+$OU = "OU=Users,DC=example,DC=com"
+$Email = "jdoe@example.com"
+$Department = "Sales"
+$Title = "Sales Executive"
+$HomeDirectory = "\\server\users\jdoe"
+
+# Create the user
+New-ADUser -Name $Username -AccountPassword $Password -PasswordNeverExpires $true -Enabled $true -DisplayName $DisplayName -Path $OU -UserPrincipalName "$Username@example.com" -SamAccountName $Username -EmailAddress $Email -Department $Department -Title $Title -HomeDirectory $HomeDirectory -HomeDrive "H:"
+
+# Add user to groups
+Add-ADGroupMember -Identity "Sales Team" -Members $Username
+Add-ADGroupMember -Identity "VPN Users" -Members $Username
