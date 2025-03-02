daa38cac20725ae25c6d7936c1c7478bd4d53557
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 09:32:12 -0600

    Restart Service
    You can define the service that needs to be restart through RMM. 
    Automate this by creating monitoring rules  when a service stops and restart the service.
    You can run a quick job and define the service that needs to be reset as well.

---
+++ Service restart automation.ps1
@@ -0,0 +1,9 @@
+﻿# Script to monitor a service and restart if stopped
+$serviceName = "ServiceName"
+$service = Get-Service -Name $serviceName
+if ($service.Status -ne "Running") {
+    Start-Service -Name $serviceName
+    Write-Output "Service $serviceName has been started."
+} else {
+    Write-Output "Service $serviceName is already running."
+}
