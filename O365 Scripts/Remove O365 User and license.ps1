ac788861600300744f7dae489f3d2e6e09173242
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 17:32:50 -0600

    Remove O365 user and license
    I find the portal function to be quicker and safer at times.  Remember, if you remove a license before converting it to a shared mailbox, then it's bad...

---
+++ O365 Scripts/Remove O365 User and license.ps1
@@ -0,0 +1,8 @@
+﻿# Define user details
+$UserPrincipalName = "user@yourdomain.com"
+
+# Remove the user
+Remove-MsolUser -UserPrincipalName $UserPrincipalName -Force
+
+# Remove the user's license
+Set-MsolUserLicense -UserPrincipalName $UserPrincipalName -RemoveLicenses "yourdomain:ENTERPRISEPACK"
