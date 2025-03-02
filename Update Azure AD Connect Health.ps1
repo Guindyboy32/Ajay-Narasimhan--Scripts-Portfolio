79dac482b4ad5a8348ec39f4f709e3938d01edc2
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 08:35:06 -0600

    Update Azure AD Connect (Health)
    Imports the Azure AD Connect Health Module to update the Sync Agent. 
    
    Useful when updating the Azure AD sync. 
    
    If you get a TLS error, I will update the script here shortly for that as well.

---
+++ Update Azure AD Connect Health.ps1
@@ -0,0 +1,14 @@
+﻿# Import the AzureADConnectHealth module
+Import-Module AzureADConnectHealth
+
+# Enable Azure AD Connect Health
+Enable-AzureADConnectHealth
+
+# Check the current status of Azure AD Connect Health
+Get-AzureADConnectHealthStatus
+
+# Register the Azure AD Connect Health Sync Agent
+Register-AzureADConnectHealthSyncAgent
+
+# Test the connectivity to Azure AD Connect Health
+Test-AzureADConnectHealthConnectivity
