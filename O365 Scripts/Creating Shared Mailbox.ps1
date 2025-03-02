31ddddf0bc21225657d983cd3e400d29222c8c27
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 17:23:52 -0600

    Creating Shared Mailboxes
    if you don't like them clicks! Just remember your Tenant's domain.  Normally as a MSP I prefer the clicks due to how many Tenants we manage.  if your managing one or two tenants, this is a cake walk!

---
+++ O365 Scripts/Creating Shared Mailbox.ps1
@@ -0,0 +1,9 @@
+﻿# Define shared mailbox details
+$SharedMailbox = "shared@yourdomain.com"
+$DisplayName = "Shared Mailbox"
+
+# Create the shared mailbox
+New-Mailbox -Shared -Name $DisplayName -PrimarySmtpAddress $SharedMailbox
+
+# Assign permissions to users
+Add-MailboxPermission -Identity $SharedMailbox -User "user@yourdomain.com" -AccessRights FullAccess -InheritanceType All
