d4721b30a04a1745b10c2f0ad58f8fb3f5ef2b01
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 09:23:46 -0600

    MSP360 Uninstall - My Version.
    There is two versions of this code.  I have uploaded my version here and there is a version that was found in the PowerShell Library, which I will upload here.  Please make sure you take a backup of the device before you uninstall.

---
+++ MSP360 Backup uninstall..ps1
@@ -0,0 +1,11 @@
+﻿# Get the MSP360 Backup application object
+$msp360 = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -like "MSP360*Backup*"}
+
+# Check if MSP360 Backup is installed
+if ($msp360) {
+    # Uninstall MSP360 Backup
+    $msp360.Uninstall()
+    Write-Host "MSP360 Backup is being uninstalled..." -ForegroundColor Green
+} else {
+    Write-Host "MSP360 Backup is not installed." -ForegroundColor Yellow
+}
