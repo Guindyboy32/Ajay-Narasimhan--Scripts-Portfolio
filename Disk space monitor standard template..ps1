bcfe841a773c35e73ccfba48a5bbecce7eaeb027
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 10:27:07 -0600

    Disk Space Monitor
    This is one of my favorites, I added this to one of my RMMs a while back. This checks for the Disk Space on the machine and sends a ticket to the support mailbox. This is great if you're not using a monitoring tool that's already built in to RMM tools.

---
+++ Disk space monitor standard template..ps1
@@ -0,0 +1,18 @@
+﻿# List of remote machines
+$machines = Get-Content -Path "C:\MSP\machines.txt"
+# Email settings
+$smtpServer = "smtp.example.com"
+$from = "alerts@example.com"
+$to = "admin@example.com"
+
+foreach ($machine in $machines) {
+    $diskSpace = Get-WmiObject Win32_LogicalDisk -ComputerName $machine -Filter "DriveType=3" | Select-Object DeviceID, @{Name="FreeSpaceGB";Expression={[math]::round($_.FreeSpace/1GB,2)}}
+    
+    foreach ($disk in $diskSpace) {
+        if ($disk.FreeSpaceGB -lt 10) { # Trigger alert if free space is less than 10GB
+            $subject = "Disk Space Alert on $machine"
+            $body = "Warning: Low disk space on $machine. Drive $($disk.DeviceID) has only $($disk.FreeSpaceGB) GB free."
+            Send-MailMessage -SmtpServer $smtpServer -From $from -To $to -Subject $subject -Body $body
+        }
+    }
+}
