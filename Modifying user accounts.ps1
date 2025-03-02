f7503a91e8f6eed3fdee4243d65716ca495f5dd9
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 10:15:32 -0600

    Modifying User accounts in AD
    The following script is a standard template to help you modify user accounts.

---
+++ Modifying user accounts.ps1
@@ -0,0 +1,8 @@
+﻿# Define user properties
+$Username = "jdoe"
+$NewDisplayName = "Johnathan Doe"
+$NewTitle = "Senior Manager"
+$NewDepartment = "Sales"
+
+# Modify the user
+Set-ADUser -Identity $Username -DisplayName $NewDisplayName -Title $NewTitle -Department $NewDepartment
