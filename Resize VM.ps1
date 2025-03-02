0fca0d8abf8b4fc03d7ad31220470a230734681f
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 11:22:57 -0600

    Resize VM - Template
    Plug in your values and test! I always test on test VM. I suggest spinning out your own test VM for you to use that you can play with:)

---
+++ Resize VM.ps1
@@ -0,0 +1,9 @@
+﻿# Define parameters
+$resourceGroupName = "MyResourceGroup"
+$vmName = "MyVM"
+$newSize = "Standard_DS3_v2"
+
+# Resize the VM
+Stop-AzVM -ResourceGroupName $resourceGroupName -Name $vmName -Force
+Update-AzVM -ResourceGroupName $resourceGroupName -VMName $vmName -Size $newSize
+Start-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
