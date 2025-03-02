fd5a4ef2b524cc3d8d0b7b87ea3ee32ac51b0a9b
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 09:29:07 -0600

    Clear Windows Update Cache.
    Clears the windows update cache as stated.  Windows stores a lot of  unneeded files once the updates are done.

---
+++ Clear Windows update Cache.ps1
@@ -0,0 +1,4 @@
+﻿# Script to clear the Windows Update cache
+Stop-Service -Name wuauserv
+Remove-Item -Path "C:\Windows\SoftwareDistribution\Download\*" -Recurse -Force
+Start-Service -Name wuauserv
