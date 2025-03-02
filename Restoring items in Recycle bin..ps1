8b14f056a8a2cd6cdcde6fe36a2efac7a212e6f9
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 09:35:06 -0600

    Restoring Items in Recycle Bin
    I found it handy in one point of time to restore the deleted items in Recycle bin. I had luck with this script. Once again, advise to save to OneDrive and SharePoint.

---
+++ Restoring items in Recycle bin..ps1
@@ -0,0 +1,9 @@
+﻿# Load the necessary .NET assembly
+Add-Type -AssemblyName Microsoft.VisualBasic
+
+# Restore all items in the Recycle Bin
+[Microsoft.VisualBasic.FileIO.FileSystem]::GetRecycleBinItems() | ForEach-Object { 
+    [Microsoft.VisualBasic.FileIO.FileSystem]::RestoreFromRecycleBin($_) 
+}
+
+Write-Host "All items have been restored from the Recycle Bin."
