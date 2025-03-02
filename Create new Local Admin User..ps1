1c706bee0436178a972c01ec3750fd09800c5c61
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 08:57:30 -0600

    Create Local Admin User
    Let me explain this code. 
    
    Say your working with a vendor, or another MSP needs access to the machine due to a handoff.  You have a guy on site when the handoff is about to happen, they need the password to install their software after the offboarding job is done.  
    
    You can create a new local admin user. 
    
    Keep in mind when you login - This will have you create a new password.  If this is  your engineer, you can save the password to IT Glue, LAPS, and SharePoint.  I will upload a code to how to remove this user in my library.

---
+++ Create new Local Admin User..ps1
@@ -0,0 +1,10 @@
+﻿# Define the username for the new local administrator account
+$username = "NewAdmin"
+ 
+# Create the new local administrator account without a password
+New-LocalUser -Name $username -NoPassword -AccountNeverExpires
+ 
+# Add the new local administrator account to the Administrators group
+Add-LocalGroupMember -Group "Administrators" -Member $username
+ 
+Write-Output "Local administrator account '$username' created successfully without a password requirement."
