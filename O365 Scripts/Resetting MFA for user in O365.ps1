59c40ede641ddf95d3b94a8204e3c9f5d14fb7cb
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 17:21:02 -0600

    Resetting MFA
    For all the people that like the clickity clicks! I love the clicks, but sometimes I don't have the time for it. Check it out by replacing the values!

---
+++ O365 Scripts/Resetting MFA for user in O365.ps1
@@ -0,0 +1,5 @@
+﻿# Define user details
+$UserPrincipalName = "user@yourdomain.com"
+
+# Enable MFA for the user
+Set-MsolUser -UserPrincipalName $UserPrincipalName -StrongAuthenticationRequirements @()
