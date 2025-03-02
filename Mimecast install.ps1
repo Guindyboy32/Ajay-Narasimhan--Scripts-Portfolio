0f768c2270fd7249e14e7bc46dadb9cf2baae4a9
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 09:42:47 -0600

    Mimecast Client install.
    I used this a while back to install the Mimecast Client.

---
+++ Mimecast install.ps1
@@ -0,0 +1,17 @@
+﻿# Define the path to the Mimecast Outlook add-on installer
+$installerPath = "Mimecast for outlook 7.10.1.133 (x64).msi"
+
+# Check if Outlook is already running and close it if necessary
+$outlookProcess = Get-Process "OUTLOOK" -ErrorAction SilentlyContinue
+if ($outlookProcess) {
+    Write-Output "Closing Outlook..."
+    $outlookProcess | ForEach-Object { $_.CloseMainWindow() | Out-Null }
+    $outlookProcess | Wait-Process
+}
+
+# Install the Mimecast add-on using the installer
+Write-Output "Installing Mimecast Outlook add-on..."
+Start-Process -FilePath msiexec.exe -ArgumentList "/i `"$installerPath`" /quiet" -Wait
+
+# Print back completion.
+Write-Output "The install is (allegedly) complete."
