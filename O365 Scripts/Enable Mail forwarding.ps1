f2c776a0a5f517d0e8c8c30933640712282acf47
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 17:58:59 -0600

    O365 Mail Forwarding
    Modify the values as you see fit!

---
+++ O365 Scripts/Enable Mail forwarding.ps1
@@ -0,0 +1,6 @@
+﻿# Define user details
+$Mailbox = "user@yourdomain.com"
+$ForwardingAddress = "forwardto@yourdomain.com"
+
+# Enable and set mailbox forwarding
+Set-Mailbox -Identity $Mailbox -ForwardingAddress $ForwardingAddress -DeliverToMailboxAndForward $true
