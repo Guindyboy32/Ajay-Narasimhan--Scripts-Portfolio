66734becd4050e6158dd8a4f1a9b90fa7869996f
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 17:34:39 -0600

    Check login for user
    You can check the sign in logs in Azure as well

---
+++ O365 Scripts/Check login for user.ps1
@@ -0,0 +1,5 @@
+﻿# Define user details
+$UserPrincipalName = "user@yourdomain.com"
+
+# Get last logon time
+Get-MailboxStatistics -Identity $UserPrincipalName | Select-Object DisplayName, LastLogonTime
