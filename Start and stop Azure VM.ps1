57ddfdd5d56705cdb17ec7ec256ea8f243f90008
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 11:35:10 -0600

    Start and Stop Azure VM
    This is fun to test out. I always prefer to click see it stop, check the logs and such.

---
+++ Start and stop Azure VM.ps1
@@ -0,0 +1,9 @@
+﻿# Define parameters
+$resourceGroupName = "MyResourceGroup"
+$vmName = "MyVM"
+
+# Start the VM
+Start-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
+
+# Stop the VM
+Stop-AzVM -ResourceGroupName $resourceGroupName -Name $vmName -Force
