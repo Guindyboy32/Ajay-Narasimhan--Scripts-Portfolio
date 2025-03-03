8af39cb48ea10c2cd935a4dfc3d276707d3a6b72
Ajay Narasimhan <simhanajay@gmail.com>
2025-03-03 16:21:32 -0600

    Automate user creation via  CSV
    One of the earliest scripts I written.

---
+++ Active Directory/Automate User creation via CSV.ps1
@@ -0,0 +1,8 @@
+﻿# Variables
+$csvPath = "C:\\Users.csv"
+$ou = "OU=Users,DC=domain,DC=com"
+
+# Import CSV and Create Users
+Import-Csv -Path $csvPath | ForEach-Object {
+    New-ADUser -Name $_.Name -GivenName $_.FirstName -Surname $_.LastName -SamAccountName $_.SamAccountName -UserPrincipalName "$($_.SamAccountName)@domain.com" -Path $ou -AccountPassword (ConvertTo-SecureString $_.Password -AsPlainText -Force) -Enabled $true
+}
