d2b694cca61f233528b63316faefad7151e5101a
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 18:04:20 -0600

    O365 - Adding users to Distro
    You can use this add multiple users to distro groups instead of the click style.

---
+++ O365 Scripts/O365 - Adding users to distro.ps1
@@ -0,0 +1,8 @@
+﻿# Define distribution group and users
+$DistributionGroup = "distributiongroup@yourdomain.com"
+$Users = @("user1@yourdomain.com", "user2@yourdomain.com")
+
+# Add users to the distribution group
+foreach ($User in $Users) {
+    Add-DistributionGroupMember -Identity $DistributionGroup -Member $User
+}
