57de416f14b9fba41d21d7329cd06b2f69d03d25
Ajay Narasimhan <simhanajay@gmail.com>
2025-03-03 16:00:19 -0600

    Start and Stop Multiple VMs using PowerShell
    This will allow you to start and stop multiple VMs as defined in the script. You will have to define the resource group as well.

---
+++ Azure VM/Start and Stop Multiple VMs using PowerShell.ps1
@@ -0,0 +1,13 @@
+﻿# Variables
+$resourceGroup = "MyResourceGroup"
+$vmNames = @("MyVM1", "MyVM2", "MyVM3")
+
+# Start VMs
+foreach ($vmName in $vmNames) {
+    Start-AzVM -ResourceGroupName $resourceGroup -Name $vmName
+}
+
+# Stop VMs
+foreach ($vmName in $vmNames) {
+    Stop-AzVM -ResourceGroupName $resourceGroup -Name $vmName -Force
+}
