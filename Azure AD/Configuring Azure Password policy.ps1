3b8ab71ec95852c0405f8e1faf90a77f3704bded
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 18:34:36 -0600

    Configuring Azure Password Policy

---
+++ Azure AD/Configuring Azure Password policy.ps1
@@ -0,0 +1,9 @@
+﻿# Define password policy details
+$PolicyName = "DefaultPasswordPolicy"
+$EnforceComplexity = $true
+$PasswordLength = 12
+$PasswordValidityPeriod = 90
+$PasswordHistoryCount = 24
+
+# Create and configure password policy
+New-AzureADMSPasswordSingleSignOnSettings -DisplayName $PolicyName -ComplexityEnabled $EnforceComplexity -MinLength $PasswordLength -ValidityPeriod $PasswordValidityPeriod -HistoryCount $PasswordHistoryCount
