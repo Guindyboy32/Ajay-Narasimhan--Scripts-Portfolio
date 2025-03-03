7386f4a2e01a86d1341916264130d281c8101061
Ajay Narasimhan <simhanajay@gmail.com>
2025-03-03 16:28:53 -0600

    Automate - Inactive accounts to preferred OU
    In this case, Disabled OU!

---
+++ Active Directory/Automate - Inactive accounts to preffered OU.ps1
@@ -0,0 +1,12 @@
+﻿# Variables
+$daysInactive = 90
+$targetOu = "OU=DisabledUsers,DC=domain,DC=com"
+
+# Find Inactive Users
+$inactiveUsers = Search-ADAccount -AccountInactive -TimeSpan (New-TimeSpan -Days $daysInactive) -UsersOnly
+
+# Disable and Move Inactive Users
+foreach ($user in $inactiveUsers) {
+    Disable-ADAccount -Identity $user
+    Move-ADObject -Identity $user.DistinguishedName -TargetPath $targetOu
+}
