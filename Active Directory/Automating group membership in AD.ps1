218f356d1719450fb95aea7196c400a706d787a4
Ajay Narasimhan <simhanajay@gmail.com>
2025-03-03 16:23:50 -0600

    Automate Group Membership
    This will allow you to automate group member ship in AD, saves time.  This also relies on a CSV.

---
+++ Active Directory/Automating group membership in AD.ps1
@@ -0,0 +1,17 @@
+﻿# Variables
+$csvPath = "C:\\UserChanges.csv"
+
+# Import CSV and Update Group Membership
+Import-Csv -Path $csvPath | ForEach-Object {
+    $user = Get-ADUser -Identity $_.SamAccountName -Properties Department
+    if ($user.Department -ne $_.NewDepartment) {
+        # Remove from Old Department Group
+        Remove-ADGroupMember -Identity "$($user.Department)Group" -Members $user.SamAccountName -Confirm:$false
+
+        # Add to New Department Group
+        Add-ADGroupMember -Identity "$($_.NewDepartment)Group" -Members $user.SamAccountName
+
+        # Update User Department
+        Set-ADUser -Identity $user.SamAccountName -Department $_.NewDepartment
+    }
+}
