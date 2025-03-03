f7e1b5cdeec43b4d1c33afe0d34503cdce16664b
Ajay Narasimhan <simhanajay@gmail.com>
2025-03-03 16:04:07 -0600

    Change VM Size and Configuration Using PowerShell
    This script will allow you to change the size and configuration of the VM when the conditions are defined.

---
+++ Azure VM/Change VM Size and Configuration Using PowerShell.ps1
@@ -0,0 +1,22 @@
+﻿# Variables
+$resourceGroup = "MyResourceGroup"
+$vmName = "MyVM"
+$newVmSize = "Standard_DS3_v2"
+$osDiskName = "NewOSDisk"
+
+# Stop VM
+Stop-AzVM -ResourceGroupName $resourceGroup -Name $vmName
+
+# Update VM Size
+$vm = Get-AzVM -ResourceGroupName $resourceGroup -Name $vmName
+$vm.HardwareProfile.VmSize = $newVmSize
+Update-AzVM -ResourceGroupName $resourceGroup -VM $vm
+
+# Update OS Disk
+$osDisk = Get-AzDisk -ResourceGroupName $resourceGroup -DiskName $osDiskName
+$vm.StorageProfile.OsDisk.ManagedDisk = New-Object Microsoft.Azure.Management.Compute.Models.ManagedDiskParameters
+$vm.StorageProfile.OsDisk.ManagedDisk.Id = $osDisk.Id
+Update-AzVM -ResourceGroupName $resourceGroup -VM $vm
+
+# Start VM
+Start-AzVM -ResourceGroupName $resourceGroup -Name $vmName
