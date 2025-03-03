b0860f6377979356ef10ddaa9669433b721c7be3
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 18:12:54 -0600

    Azure AD Conditional Access Policy
    Learn how Azure works before implementing PowerShell Scripts.

---
+++ Azure AD/Configuring Conditional Access.ps1
@@ -0,0 +1,14 @@
+﻿# Define policy details
+$PolicyName = "DefaultPolicy"
+$UserGroup = "All Users"
+$Condition = "All Applications"
+
+# Create a conditional access policy
+New-AzureADMSConditionalAccessPolicy -DisplayName $PolicyName -State "Enabled" -Conditions @{
+    Users = @{
+        IncludeUsers = @("All")
+    }
+    Applications = @{
+        IncludeApplications = @("All")
+    }
+}
