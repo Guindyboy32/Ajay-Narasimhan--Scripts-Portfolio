2f322a9200e11c4f4e2c4f2a32d9360beefb3d7e
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 18:19:34 -0600

    O365 - Company wide OOO
    This one is fun, I pulled it off for a lot of clients. Replace the message with what you need to say:)

---
+++ O365 Scripts/Office 365 - Company wide OOO.ps1
@@ -0,0 +1,11 @@
+﻿# Define out of office message
+$Subject = "Out of Office"
+$Message = "Thank you for your email. I am currently out of the office and will get back to you as soon as possible."
+
+# Get all users
+$Users = Get-MsolUser -All
+
+# Set out of office message for each user
+foreach ($User in $Users) {
+    Set-MailboxAutoReplyConfiguration -Identity $User.UserPrincipalName -AutoReplyState Enabled -InternalMessage $Message -ExternalMessage $Message -Subject $Subject
+}
