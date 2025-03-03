dede6e2f068087583ec670865ae5e9de737f5088
Ajay Narasimhan <simhanajay@gmail.com>
2025-03-03 16:12:10 -0600

    Automate VM shutdown
    This will allow you to schedule a shutdown of the VM at  an designated time

---
+++ Azure VM/automate VM shutdown.ps1
@@ -0,0 +1,11 @@
+﻿# Variables
+$resourceGroup = "MyResourceGroup"
+$vmName = "MyVM"
+$shutdownTime = "1800"  # Time in HHMM format (24-hour clock)
+$timeZone = "UTC"
+
+# Enable Auto-Shutdown
+$vm = Get-AzVM -ResourceGroupName $resourceGroup -Name $vmName
+$vm.Tags["AutoShutdownTime"] = $shutdownTime
+$vm.Tags["AutoShutdownTimeZone"] = $timeZone
+Update-AzVM -ResourceGroupName $resourceGroup -VM $vm
