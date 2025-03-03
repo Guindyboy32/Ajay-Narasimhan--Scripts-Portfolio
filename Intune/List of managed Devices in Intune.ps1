af0854248fbe7c6ee85f77b415160def1b87bb05
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 18:29:54 -0600

    List of Managed devices exported to CSV
    Exports managed devices to CSV

---
+++ Intune/List of managed Devices in Intune.ps1
@@ -0,0 +1,2 @@
+﻿# Get all devices and export to CSV
+Get-IntuneManagedDevice | Select-Object DeviceName, OperatingSystem, ComplianceState | Export-Csv -Path "C:\Users\YourUsername\Documents\DeviceInventory.csv" -NoTypeInformation
