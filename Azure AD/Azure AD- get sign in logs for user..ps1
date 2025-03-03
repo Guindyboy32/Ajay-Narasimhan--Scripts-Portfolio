b8e17282b2d29a0217ed7e9cd4b9016693d8727a
Guindyboy32 <simhanajay@gmail.com>
2025-03-02 18:32:59 -0600

    Azure AD- get sign in logs for user
    All those pesky times you have to filter through the Azure AD sign in logs search, that can be avoided.

---
+++ Azure AD/Azure AD- get sign in logs for user..ps1
@@ -0,0 +1,5 @@
+﻿# Define user details
+$UserPrincipalName = "user@yourdomain.com"
+
+# Get sign-in logs for the user
+Get-AzureADAuditSignInLogs -Filter "userPrincipalName eq '$UserPrincipalName'" | Select-Object UserPrincipalName, AppDisplayName, CreatedDateTime, IPAddress, Status | Export-Csv -Path "C:\Users\YourUsername\Documents\SignInLogs.csv" -NoTypeInformation
