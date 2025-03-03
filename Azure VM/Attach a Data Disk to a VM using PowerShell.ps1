e81294475fcb34a5cd5d16f549932f2e4f52cc67
Ajay Narasimhan <simhanajay@gmail.com>
2025-03-03 12:42:34 -0600

    Attach a Data Disk to a VM using PowerShell
    Attach a Data Disk to a VM using PowerShell, you will need to modify the values needed.

---
+++ Azure VM/Attach a Data Disk to a VM using PowerShell.ps1
@@ -0,0 +1,23 @@
+﻿# Variables
+$resourceGroup = "MyResourceGroup"
+$vmName = "MyVM"
+$location = "EastUS"
+$dataDiskName = "MyDataDisk"
+$diskSizeGB = 128
+
+# Create Managed Disk
+$dataDisk = New-AzDisk `
+  -ResourceGroupName $resourceGroup `
+  -DiskName $dataDiskName `
+  -Disk `
+  -DiskSizeGB $diskSizeGB `
+  -Location $location
+
+# Get VM
+$vm = Get-AzVM -ResourceGroupName $resourceGroup -VMName $vmName
+
+# Add Data Disk to VM
+$vm = Add-AzVMDataDisk -VM $vm -Name $dataDiskName -ManagedDiskId $dataDisk.Id -Caching ReadWrite -Lun 0 -CreateOption Attach
+
+# Update VM
+Update-AzVM -VM $vm -ResourceGroupName $resourceGroup
