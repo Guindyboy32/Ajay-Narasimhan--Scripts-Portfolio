f58ebe293813531ea6ab82c9b8b130f6ac2cefb9
Ajay Narasimhan <simhanajay@gmail.com>
2025-03-03 16:07:47 -0600

    Moving VM to different resource group
    This script moves the VM to a different resource group.

---
+++ Azure VM/Move a VM to a Different Resource Group using PowerShell.ps1
@@ -0,0 +1,7 @@
+﻿# Variables
+$sourceResourceGroup = "MySourceResourceGroup"
+$destinationResourceGroup = "MyDestinationResourceGroup"
+$vmName = "MyVM"
+
+# Move VM
+Move-AzResource -DestinationResourceGroupName $destinationResourceGroup -ResourceId "/subscriptions/$subscriptionId/resourceGroups/$sourceResourceGroup/providers/Microsoft.Compute/virtualMachines/$vmName"
