0a14d1bc801df7f77c9b5d6cd637d4a6ae0c7ec7
Ajay Narasimhan <simhanajay@gmail.com>
2025-03-05 08:49:58 -0600

    Automation- How to Assign scripts, apps (OOBE)
    #For example, you can use this script to assign applications during Autopilot deployment
    #This will allow you to assign Scripts to device groups for OOBE Experience

---
+++ Autopilot/Assigning applications, scripts for AP deployment (OOBE).ps1
@@ -0,0 +1,12 @@
+﻿#For example, you can use this script to assign applications during Autopilot deployment
+#This will allow you to assign Scripts to device groups for OOBE Experience
+
+# Example script to install an application
+Install-PackageProvider -Name NuGet -Force
+Install-Module -Name PSWindowsUpdate -Force
+Import-Module PSWindowsUpdate
+Get-WindowsUpdate -Install -AcceptAll -AutoReboot
+
+#This script will install the updates during OOBE
+
+
