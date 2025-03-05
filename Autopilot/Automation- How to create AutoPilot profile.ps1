e72a49fceef51e8b53b122eb8eacde57efb8d2c1
Ajay Narasimhan <simhanajay@gmail.com>
2025-03-05 08:56:11 -0600

    Automation- How to create AutoPilot profile
    # This scripts connects to Microsoft Graph.  
    # I will explain this further, you will need to have permissions needed to manage AutoPilot profiles.

---
+++ Autopilot/Automation- How to create AutoPilot profile.ps1
@@ -0,0 +1,23 @@
+﻿# This scripts connects to Microsoft Graph.  
+# I will explain this further, you will need to have permissions needed to manage AutoPilot profiles. 
+
+# Connect to Microsoft Graph
+Install-Module -Name Microsoft.Graph -Force
+Connect-MgGraph -Scopes "DeviceManagementConfiguration.ReadWrite.All"
+
+# Create AutoPilot Profile
+$params = @{
+    displayName          = "Standard Deployment"
+    description          = "Deployment profile for standard users."
+    enrollmentStatusPage = @{
+        isConfigured = $true
+    }
+    language             = "en-US"
+    outOfBoxExperience   = @{
+        hideEscapeOption        = $true
+        hideEULA                = $true
+        userType                = "Standard"
+    }
+}
+
+New-MgDeviceManagementDeploymentProfile -BodyParameter $params
