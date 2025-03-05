1c03ae9b61ea37dfefb5729efc431af7718d6b1b
Ajay Narasimhan <simhanajay@gmail.com>
2025-03-05 10:00:33 -0600

    Retrieve AP Deployment profile information
    #This script will retrieve AutoPilot profile. 
    #This script will display the devices in table format

---
+++ Autopilot/Retrieve AP Deployment profile information.ps1
@@ -0,0 +1,10 @@
+﻿# Get AutoPilot profiles
+Install-Module -Name Microsoft.Graph.DeviceManagement -Force
+Connect-MgGraph -Scopes "DeviceManagementConfiguration.Read.All"
+
+$profiles = Get-MgDeviceManagementDeploymentProfile
+$profiles | Format-Table displayName, description, outOfBoxExperience
+
+
+#This script will retrieve AutoPilot profile. 
+#This script will display the devices in table format
