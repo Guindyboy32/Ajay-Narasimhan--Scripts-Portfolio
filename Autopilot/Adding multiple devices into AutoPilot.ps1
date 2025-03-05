d137ffae9fc4962a2a9f38290b7b850055138cc6
Ajay Narasimhan <simhanajay@gmail.com>
2025-03-05 09:48:01 -0600

    Adding multiple devices into AutoPilot
    # You will need to replace (Add-AutoPilotDevice) given by the MGraph API or Intune PShell Module.

---
+++ Autopilot/Adding multiple devices into AutoPilot.ps1
@@ -0,0 +1,11 @@
+﻿# Import CSV and upload devices
+$csvFile = "C:\path\to\devices.csv"
+$deviceList = Import-Csv -Path $csvFile
+
+foreach ($device in $deviceList) {
+    Add-AutoPilotDevice -SerialNumber $device.SerialNumber -HardwareHash $device.HardwareHash
+}
+
+
+
+# You will need to replace (Add-AutoPilotDevice) given by the MGraph API or Intune PShell Module. 
