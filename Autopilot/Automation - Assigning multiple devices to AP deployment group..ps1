4d46844db40e83ce41c8f298e6b4c4552adf3ea1
Ajay Narasimhan <simhanajay@gmail.com>
2025-03-05 09:05:17 -0600

    Automate-Assign multiple devices to AP deploy grp
    ## You can assign devices to the AP deployment group. Makes work eaiser, when you have to add multiple devices. 
    ## You will need to replace the add-devicetogroup function if architecture is combined with Azure AD and Intune.

---
+++ Autopilot/Automation - Assigning multiple devices to AP deployment group..ps1
@@ -0,0 +1,12 @@
+﻿# Example to assign devices to a group
+$groupName = "AutoPilot Devices Group"
+$deviceSerials = @("SerialNumber1", "SerialNumber2")
+
+foreach ($serial in $deviceSerials) {
+    Add-DeviceToGroup -DeviceSerial $serial -GroupName $groupName
+}
+
+
+
+## You can assign devices to the AP deployment group. Makes work eaiser, when you have to add multiple devices. 
+## You will need to replace the add-devicetogroup function if architecture is combined with Azure AD and Intune.   
