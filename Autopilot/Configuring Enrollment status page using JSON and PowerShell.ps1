0d56b10c69184e7bbc8ee12fa34e8634fb68de41
Ajay Narasimhan <simhanajay@gmail.com>
2025-03-05 09:54:12 -0600

    Configure Enrollment page using JSON and PS
    Please understand this uses Microsoft Graph, and the understanding of JSON. This invokes the Graph API call using the URL. I would spend time reading how JSON and API works.

---
+++ Autopilot/Configuring Enrollment status page using JSON and PowerShell.ps1
@@ -0,0 +1,10 @@
+﻿# Sample JSON body for the API
+$ESPConfig = @{
+    displayName = "Default ESP"
+    enableCustomMessages = $true
+    customMessage = "Your device is being configured. Please wait..."
+    blockDeviceUseUntilProfileApplied = $true
+}
+
+# Call Microsoft Graph API to set ESP
+Invoke-MgGraphRequest -Uri "https://graph.microsoft.com/v1.0/deviceManagement/enrollmentStatusPages" -Method POST -Body $ESPConfig
