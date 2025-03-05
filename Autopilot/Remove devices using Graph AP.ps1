dc44e6d53c28e18fa63ee8130c13f36fd49c8073
Ajay Narasimhan <simhanajay@gmail.com>
2025-03-05 10:04:10 -0600

    Remove devices using Graph AP
    You will need to replace (DeviceIDtoRemove) with the device that you want to remove.
    This uses Mgraph
    You can use this to deregister devices

---
+++ Autopilot/Remove devices using Graph AP.ps1
@@ -0,0 +1,10 @@
+﻿# Remove device
+Install-Module -Name Microsoft.Graph.DeviceManagement -Force
+Connect-MgGraph -Scopes "DeviceManagementConfiguration.ReadWrite.All"
+
+Remove-MgDeviceManagementWindowsAutopilotDeviceIdentity -DeviceId "DeviceIDToRemove"
+
+
+#You will need to replace (DeviceIDtoRemove) with the device that you want to remove.
+#This uses Mgraph
+#You can use this to deregister devices. 
