f6db25585d8cd9c2d10b7708b2527e35d4685329
Ajay Narasimhan <simhanajay@gmail.com>
2025-03-05 09:14:05 -0600

    How to export devices from AP
    ## Make sure you have the Intune permissons to run this. 
    #This will export all the devices that you have registered to CSV File.

---
+++ Autopilot/How to export devices from AP.ps1
@@ -0,0 +1,10 @@
+﻿# Export devices
+Install-Module -Name Microsoft.Graph.Intune -Force
+Connect-MSGraph
+
+$devices = Get-AutoPilotRegisteredDevices
+$devices | Export-Csv -Path "AutoPilotDevices.csv" -NoTypeInformation -Encoding UTF8
+
+
+## Make sure you have the Intune permissons to run this. 
+#This will export all the devices that you have registered to CSV File. 
