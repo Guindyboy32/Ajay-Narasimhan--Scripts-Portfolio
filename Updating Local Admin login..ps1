36617c44d7a488f2b8ec99c1d634200bf15e7408
Ajay Narasimhan <simhanajay@gmail.com>
2025-03-10 10:47:43 -0500

    Creating LocalAdmin account with password config
    Alright Y'all I tested this today. I changed some info on here to better fit your organization.  Remember the password values have to be changed for your ORG.

---
+++ Updating Local Admin login..ps1
@@ -0,0 +1,11 @@
+﻿# Define the username and password for the new local administrator account
+$username = "NewAdmin"
+$password = ConvertTo-SecureString "Enterpasswordhere!" -AsPlainText -Force
+
+# Create the new local administrator account with the password
+New-LocalUser -Name $username -Password $password -AccountNeverExpires
+
+# Add the new local administrator account to the Administrators group
+Add-LocalGroupMember -Group "Administrators" -Member $username
+
+Write-Output "Local administrator account '$username' created successfully with the password 'Enterpasswordhere'."
