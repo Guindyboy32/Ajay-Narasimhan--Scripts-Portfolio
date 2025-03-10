a61c4b2e8bb0a0bb176d085ffe1e350170368eaa
Ajay Narasimhan <simhanajay@gmail.com>
2025-03-10 16:17:54 -0500

    Automate - OneDrive Folder Redirection
    This is a improved version of the script that I did earlier today.

---
+++ OneDrive/Automate - OneDrive Folder Redirection.ps1
@@ -0,0 +1,15 @@
+﻿# Define OneDrive path and folders to redirect
+$OneDrivePath = "$env:USERPROFILE\OneDrive"
+$FoldersToRedirect = @("Desktop", "Documents", "Pictures", "Downloads")
+
+foreach ($Folder in $FoldersToRedirect) {
+    $CurrentPath = Join-Path $env:USERPROFILE $Folder
+    $NewPath = Join-Path $OneDrivePath $Folder
+
+    if (-not (Test-Path -Path $NewPath)) {
+        New-Item -ItemType Directory -Path $NewPath
+    }
+
+    New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name $Folder -Value $NewPath -Force
+}
+Write-Output "Selected folders redirected to OneDrive."
