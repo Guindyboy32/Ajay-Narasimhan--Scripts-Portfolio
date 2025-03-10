da90721bc5e9cac3e43a0dba96cc39122418c532
Ajay Narasimhan <simhanajay@gmail.com>
2025-03-10 16:06:13 -0500

    OneDrive Silent Sync
    On the the days where you don't have to bother the user when the OneDrive is not syncing.

---
+++ OneDrive/OneDrive Silent sync.ps1
@@ -0,0 +1,4 @@
+﻿# Start OneDrive sync
+Stop-Process -Name "OneDrive" -Force -ErrorAction SilentlyContinue
+Start-Process -FilePath "$env:LOCALAPPDATA\Microsoft\OneDrive\OneDrive.exe" -ArgumentList "/syncnow"
+Write-Output "OneDrive sync initiated."
