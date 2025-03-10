07d0edbb5e31dc90ad33cb3a2a5da74da0f7a136
Ajay Narasimhan <simhanajay@gmail.com>
2025-03-10 16:09:04 -0500

    OneDrive Checking sync status
    You can use this to check if the OneDrive client is running.  I prefer to use PowerShell instead of clicking.

---
+++ OneDrive/OneDrive Checking sync status.ps1
@@ -0,0 +1,8 @@
+﻿# Check for OneDrive process
+$OneDriveProcess = Get-Process -Name "OneDrive" -ErrorAction SilentlyContinue
+
+if ($OneDriveProcess) {
+    Write-Output "OneDrive is running."
+} else {
+    Write-Output "OneDrive is not running. Please start OneDrive."
+}
