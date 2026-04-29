aba9a68e6002fe6840e50991157d9f6449e020db
Ajay Narasimhan <simhanajay@gmail.com>
2026-04-29 16:17:23 -0700

    Add script for converting to shared mailbox

---
+++ Exchange OnPrem/Converting to Shared Mailbox.ps1
@@ -0,0 +1,7 @@
+#Single user
+Set-Mailbox -Identity "username" -Type Shared
+
+### Bulk - Uses CSV.
+Import-Csv "mailboxes.csv" | ForEach-Object {
+    Set-Mailbox -Identity $_.Mailbox -Type Shared
+}
