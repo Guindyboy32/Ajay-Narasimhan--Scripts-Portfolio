8ada328d54e6c30a0eebe275d9782e06832da667
Ajay Narasimhan <simhanajay@gmail.com>
2026-05-04 16:09:43 -0700

    Delete Intune/List of managed Devices in Intune.ps1

--- Intune/List of managed Devices in Intune.ps1
+++
@@ -1,2 +0,0 @@
-﻿# Get all devices and export to CSV
-Get-IntuneManagedDevice | Select-Object DeviceName, OperatingSystem, ComplianceState | Export-Csv -Path "C:\Users\YourUsername\Documents\DeviceInventory.csv" -NoTypeInformation
